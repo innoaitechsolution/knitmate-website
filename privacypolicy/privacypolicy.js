13a7a9dbe5dd91e5f8ff03b293a18aa343166006
Arzu Caner <arzuguneycaner@gmail.com>
2024-09-06 19:37:21 +0100

    Add privacypolicy

---
+++ privacypolicy/privacypolicy.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const PrivacyPolicy = () => {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif', lineHeight: '1.6' }}>
+      <h1>Privacy Policy</h1>
+      <p>Last Updated: 11.08.2024</p>
+      
+      <p>This privacy policy explains how Knitt Mates handles user privacy.</p>
+
+      <h2>No Information Collection</h2>
+      <p>Our app does not collect any personal information. No names, email addresses, phone numbers, or any other personal information are collected, stored, or shared by this app.</p>
+
+      <h2>Cookies</h2>
+      <p>Our app does not use cookies. Cookies are small pieces of data sent to the browser by websites to improve user experience. Since our app does not use cookies, no data is collected or stored.</p>
+
+      <h2>Third-Party Services</h2>
+      <p>Our app does not use any third-party services. Therefore, no user data is shared with third parties.</p>
+
+      <h2>Data Security</h2>
+      <p>Since no user data is collected, there are no risks related to data security. Our app functions solely to assist users with yarn calculations.</p>
+
+      <h2>Changes</h2>
+      <p>This privacy policy may be updated from time to time. Changes to our privacy policy will be posted on this page. Users are encouraged to check this page regularly to stay informed of any changes.</p>
+
+      <h2>Contact</h2>
+      <p>If you have any questions about this privacy policy, please contact us at <a href="mailto:knittmates@gmail.com">knittmates@gmail.com</a>.</p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
