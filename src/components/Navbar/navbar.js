a5b0a2093273b0548b8850db7e97843567c3a192
Arzu Caner <arzuguneycaner@gmail.com>
2025-01-20 22:42:20 +0000

    Update navbar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaTwitter, FaYoutube, FaTiktok } from 'react-icons/fa'; 
 import './Navbar.css';
 
 const Navbar = () => {
@@ -36,14 +37,29 @@ const Navbar = () => {
 
       {/* Social Media Links */}
       <div className="navbar__social">
-        <a href="https://x.com/knittmates" target="_blank" rel="noopener noreferrer">
-          Twitter (X)
+        <a
+          href="https://x.com/knittmates"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Twitter"
+        >
+          <FaTwitter />
         </a>
-        <a href="https://www.youtube.com/@KnittMates" target="_blank" rel="noopener noreferrer">
-          YouTube
+        <a
+          href="https://www.youtube.com/@KnittMates"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="YouTube"
+        >
+          <FaYoutube />
         </a>
-        <a href="https://www.tiktok.com/@knittmates?lang=en" target="_blank" rel="noopener noreferrer">
-          TikTok
+        <a
+          href="https://www.tiktok.com/@knittmates?lang=en"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="TikTok"
+        >
+          <FaTiktok />
         </a>
       </div>
     </nav>
