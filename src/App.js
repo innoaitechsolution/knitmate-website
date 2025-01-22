1446890ccfc77f59bc2d5d64e44d7c09753ff486
Arzu Caner <arzuguneycaner@gmail.com>
2025-01-22 11:53:13 +0000

    Fix the project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/Navbar/navbar';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
 import About from './components/About/About';
